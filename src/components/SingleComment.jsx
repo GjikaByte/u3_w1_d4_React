97a50871baa253cff8474c76681b6b56b73d92dc
Andi Gjika <andi.gjika@outlook.com>
2025-10-02 17:49:50 +0200

    Amended SingleComments.jsx

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -4,6 +4,7 @@ export default function SingleComment({ comment }) {
   // comment: { _id, comment, rate, elementId, ... }
   return (
     <ListGroup.Item className="d-flex justify-content-between align-items-start">
+      <strong>{comment.author}</strong>
       <span>{comment.comment}</span>
       <small>⭐ {comment.rate}</small>
     </ListGroup.Item>
